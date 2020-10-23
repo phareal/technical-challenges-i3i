e0d1d6dee135ae658b872434761db1275ad92d74
CornerstoneII <seunmyles@gmail.com>
2020-10-23 13:22:10 +0100

    Add client ID and secret to config

--- spotify-react/src/config.js
+++ spotify-react/src/config.js
@@ -2,7 +2,7 @@ export default {
   api: {
     baseUrl: 'https://api.spotify.com/v1',
     authUrl: 'https://accounts.spotify.com/api/token',
-    clientId: '',
-    clientSecret: ''
+    clientId: '944d9f35eef94872bbfe28ef0c424c0e',
+    clientSecret: '262b53d0cd364a7bb0ba3c4aa337a1e3'
   }
 }
